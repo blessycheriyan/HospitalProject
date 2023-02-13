3d80010c839c44d039e15458e19ba1429567da3f
Blessy <blessysusancheriyan28@gmail.com>
2023-02-13 11:02:11 +0530

    first

--- project/settings.py
+++ project/settings.py
@@ -117,14 +117,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR,'static')
+
+# ]
+# STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
-
 ]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
-
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
